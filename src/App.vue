eef8ecaacfd3d0167e0cc995280394ed46b68410
Wyatt Nolen <wyattnolen@gmail.com>
2020-08-06 00:52:01 -0400

    shuffle deck

--- src/App.vue
+++ src/App.vue
@@ -26,15 +26,33 @@ export default {
           title: "Investigators"
         }
       },
-      deck: ["0", "1", "2"],
+      deck: ["0", "1", "2"]
     };
   },
   created() {
-    this.displayInitialMap();
+    this.start();
   },
   methods: {
-    displayInitialMap() {
-      console.log(this.cards);
+    start() {
+      this.shuffleDeck();
+    },
+    shuffleDeck() {
+      var array = this.deck;
+      var currentIndex = array.length,
+        temporaryValue,
+        randomIndex;
+
+      // While there remain elements to shuffle...
+      while (0 !== currentIndex) {
+        // Pick a remaining element...
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex -= 1;
+
+        // And swap it with the current element.
+        temporaryValue = array[currentIndex];
+        array[currentIndex] = array[randomIndex];
+        array[randomIndex] = temporaryValue;
+      }
     }
   }
 };
