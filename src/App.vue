176aef01a18b04071b22d0458670e548eb1cf7f4
Wyatt Nolen <wyattnolen@gmail.com>
2020-08-06 00:27:32 -0400

    Update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,43 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
     <router-view />
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  name: "App",
+  data: function() {
+    return {
+      cards: {
+        c0: {
+          id: "0",
+          value: "0",
+          title: "Mi-Go Braincase"
+        },
+        c1: {
+          id: "1",
+          value: "1",
+          title: "Investigators"
+        },
+        c2: {
+          id: "2",
+          value: "1",
+          title: "Investigators"
+        }
+      },
+      deck: ["0", "1", "2"],
+    };
+  },
+  created() {
+    this.displayInitialMap();
+  },
+  methods: {
+    displayInitialMap() {
+      console.log(this.cards);
     }
   }
-}
-</style>
+};
+</script>
+
+<style lang="scss"></style>
